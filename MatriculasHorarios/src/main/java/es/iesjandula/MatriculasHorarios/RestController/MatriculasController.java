e9882310940ee87e7cebacdb91eb4bd2b67eaa82
manumartin0709 <113683687+manumartin0709@users.noreply.github.com>
2024-12-04 13:25:42 +0100

    Update MatriculasController.java

--- MatriculasHorarios/src/main/java/es/iesjandula/MatriculasHorarios/RestController/MatriculasController.java
+++ MatriculasHorarios/src/main/java/es/iesjandula/MatriculasHorarios/RestController/MatriculasController.java
@@ -1,41 +1,41 @@
 package es.iesjandula.MatriculasHorarios.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+import org.apache.tomcat.util.json.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import es.iesjandula.MatriculasHorarios.models.AlumnoEntity;
 import es.iesjandula.MatriculasHorarios.models.CursoEtapaEntity;
-import es.iesjandula.MatriculasHorarios.models.MatriculaEntity;
-import es.iesjandula.MatriculasHorarios.repositories.IAlumnoRepository;
+import es.iesjandula.MatriculasHorarios.models.DatosBrutoAlumnoMatriculaEntity;
+import es.iesjandula.MatriculasHorarios.models.ids.IdCursoEtapa;
 import es.iesjandula.MatriculasHorarios.repositories.ICursoEtapaRepository;
-import es.iesjandula.MatriculasHorarios.repositories.IMatriculaRepository;
+import es.iesjandula.MatriculasHorarios.repositories.IDatosBrutoAlumnoMatricula;
 
 @RestController
 @RequestMapping(value = "/Matriculas")
-public class MatriculasController {
-	
-	
-	@Autowired
-	private IMatriculaRepository iMatriculaRepository;
-	
-	@Autowired
-	private IAlumnoRepository iAlumnoRepository;
-	
+public class MatriculasController 
+{
 	@Autowired
 	private ICursoEtapaRepository iCursoRepository;
+	
+	@Autowired
+	private IDatosBrutoAlumnoMatricula iDatosBrutoAlumnoMatricula;
 	
 	
 	@RequestMapping( method = RequestMethod.POST,value = "/upload",consumes = "multipart/form-data")
@@ -45,77 +45,104 @@ public class MatriculasController {
 			)
 	{
 		
-		Scanner scanner;
-		
+		Scanner scanner = null;
 		try
 		{
-			
-			
 			scanner = new Scanner(new File("src/main/resources/cursos.csv"));
 			// PARSEAMOS LA FECHA CON UN FORMATO ESPECIFICO
 			scanner.nextLine();
 			while (scanner.hasNextLine())
 			{
-				CursoEtapaEntity cursoEtapaEntity =  new CursoEtapaEntity();
+				IdCursoEtapa idCursoEtapa         = new IdCursoEtapa();
+				CursoEtapaEntity cursoEtapaEntity = new CursoEtapaEntity();
 				String line = scanner.nextLine();
 				StringTokenizer tokenizer = new StringTokenizer(line, ",");
-				cursoEtapaEntity.setDatosBrutosAlumnosMatriculados(tokenizer.nextToken());		
+				idCursoEtapa.setCurso(Integer.parseInt(tokenizer.nextToken()));
+				idCursoEtapa.setEtapa(tokenizer.nextToken());
+				cursoEtapaEntity.setIdCursoEtapa(idCursoEtapa);
 				this.iCursoRepository.saveAndFlush(cursoEtapaEntity);
 			}
-
-			scanner.close();
 		}
 		catch(Exception exception)
 		{
-			log.error("Error de servidor",exception);
+			
 			return ResponseEntity.status(500).body("Error de servidor"+exception);
 		}
-		
+		finally 
+		{
+			scanner.close();
+		}
 		return ResponseEntity.ok().body(200);
 	}
 	
+	/**
+	 * Endpoint que se encarga de subir los alumnos a la base de datos cargados desde el csv
+	 * @param curso
+	 * @param etapa
+	 * @param csv
+	 */
+	@RequestMapping(method = RequestMethod.POST,value = "/cargaAlumnos", consumes = "multipart/form-data")
+	public void cargaMatriculas(
+								@RequestHeader(required = true) int curso,
+								@RequestHeader(required = true) String etapa,
+								@RequestPart( value = "csv") MultipartFile csv 
+			)
+	{
+		Scanner scanner = null;
+		IdCursoEtapa idCursoEtapa         = new IdCursoEtapa();
+		CursoEtapaEntity cursoEtapaEntity = new CursoEtapaEntity();
+		DatosBrutoAlumnoMatriculaEntity datosBrutoAlumnoMatriculaEntity = new DatosBrutoAlumnoMatriculaEntity();
+		try 
+		{
+			//rellenamos el la clave primaria de la tabla CursoEntity
+			idCursoEtapa.setCurso(curso);
+			idCursoEtapa.setEtapa(etapa);
+			cursoEtapaEntity.setIdCursoEtapa(idCursoEtapa);
+			scanner = new Scanner(new File("src/main/resources/alumnos.csv"));
+			// PARSEAMOS LA FECHA CON UN FORMATO ESPECIFICO
+			scanner.nextLine();
+			while (scanner.hasNextLine())
+			{
+				
+				String line = scanner.nextLine();
+				StringTokenizer tokenizer = new StringTokenizer(line, ",");
+				datosBrutoAlumnoMatriculaEntity.setNombre(tokenizer.nextToken());
+				datosBrutoAlumnoMatriculaEntity.setApellidos(tokenizer.nextToken());
+				datosBrutoAlumnoMatriculaEntity.setAsignatura(tokenizer.nextToken());
+				datosBrutoAlumnoMatriculaEntity.setCursoEtapa(cursoEtapaEntity);
+				this.iDatosBrutoAlumnoMatricula.saveAndFlush(datosBrutoAlumnoMatriculaEntity); 	
+			}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally 
+		{
+			scanner.close();
+		}
+		
+	}
 	
+	/**
+	 * 
+	 */
 	@RequestMapping(method = RequestMethod.GET,value = "/Cursos")
 	public ResponseEntity <?> obtenerCursos() 
 	{
 		List<CursoEtapaEntity>listaCursos = this.iCursoRepository.findAll();
 		return ResponseEntity.status(200).body(listaCursos);
 	}
-	
+	/**
+	 * 
+	 */
 	@RequestMapping(method = RequestMethod.GET,value = "/Alumnos")
-	public ResponseEntity <?> obtenerAlumnos(@RequestBody(required = true) MultipartFile fichero) throws StreamReadException, DatabindException, IOException{
-		
-		List<AlumnoEntity> alumnos= this.iAlumnoRepository.findAll();
-		return ResponseEntity.status(200).body(alumnos);
-	}
-	
-	@RequestMapping(method = RequestMethod.POST,value = "/cargaAlumnos")
-	public void cargaMatriculas(@RequestHeader(required = true) String curso,
-								@RequestHeader(required = true) String etapa
-			)
+	public ResponseEntity <?> obtenerAlumnos()
 	{
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()) 
-		{
-			String linea = scanner.nextLine();
-			
-			String [] valores = linea.split(",");
-			
-			Alumno alumno = new Alumno();
-			
-			alumno.setNombre(valores[1]);
-			
-			alumno.setNombre(valores[0]);
-			
-			this.iAlumnoRepository.saveAllAndFlush(alumno);
-			
-			
-			
-						
-					 			
-		}
+		List<DatosBrutoAlumnoMatriculaEntity> datosBrutoAlumnoMatriculaList= this.iDatosBrutoAlumnoMatricula.findAll();
+		return ResponseEntity.status(200).body(datosBrutoAlumnoMatriculaList);
 	}
-	
 
 }
 
